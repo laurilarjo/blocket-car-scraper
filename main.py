9397fe44fb4e371ed7e0d1dae8ea484442f01c10
Lauri Larjo <laurilarjo@gmail.com>
2020-05-23 17:16:32 +0200

    Catching exceptions so crawler doesn't stop to formatting errors

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import traceback
 import scrapy
+
 
 # Run this file:
 # scrapy runspider main.py -o cars.csv
@@ -7,10 +9,11 @@ import scrapy
 # scrapy shell "https://www.blocket.se/annonser/stockholm/fordon/bilar?cb=41&cbl1=10&cg=1020&mye=2017&mys=2010&r=11"
 
 SEARCH_PAGE = 'https://www.blocket.se/annonser/stockholm/fordon/bilar?cb=41&cbl1=10&cg=1020&mye=2017&mys=2010&r=11'
+V70 = 'https://www.blocket.se/annonser/hela_sverige/fordon/bilar?cb=41&cbl1=10&cg=1020&mye=2020&mys=2008'
 
 class BlocketCarSpider(scrapy.Spider):
     name = 'blocketcarspider'
-    start_urls = [SEARCH_PAGE]
+    start_urls = [V70]
     custom_settings = {
         # 'LOG_FILE': 'log.txt',
         'FORMAT' : 'csv',
@@ -21,19 +24,24 @@ class BlocketCarSpider(scrapy.Spider):
     def parse(self, response):
         for car in response.xpath('//*[@id="root"]/div[2]/main/div[3]/div[1]/div[2]/div[1]/div'):
             if (car.xpath('article/div[2]/div[2]/h2/a/span/text()').get()):
-                yield {
-                    'title' : car.xpath('article/div[2]/div[2]/h2/a/span/text()').get(),
+                try:
+                    yield {
+                        'title' : car.xpath('article/div[2]/div[2]/h2/a/span/text()').get(),
 
-                    'year' : car.xpath('article/div[2]/div[last()-1]/div[1]/ul/li[1]/text()').get(),
-                    'gastype' : car.xpath('article/div[2]/div[last()-1]/div[1]/ul/li[2]/text()').get(),
-                    'miles' : self.formatMiles(car.xpath('article/div[2]/div[last()-1]/div[1]/ul/li[3]/text()').get()),
-                    'transmission' : car.xpath('article/div[2]/div[last()-1]/div[1]/ul/li[4]/text()').get(),
+                        'year' : car.xpath('article/div[2]/div[last()-1]/div[1]/ul/li[1]/text()').get(),
+                        'gastype' : car.xpath('article/div[2]/div[last()-1]/div[1]/ul/li[2]/text()').get(),
+                        'miles' : self.formatMiles(car.xpath('article/div[2]/div[last()-1]/div[1]/ul/li[3]/text()').get()),
+                        'transmission' : car.xpath('article/div[2]/div[last()-1]/div[1]/ul/li[4]/text()').get(),
 
-                    'price' : self.formatPrice(car.xpath('article/div[2]/div[last()]/div/div/span/text()').get()),
-                    'company' : self.formatCompany(car.xpath('article/div[2]/div[1]/div[1]/span/text()').get()),
-                    'location' : car.xpath('article/div[2]/div[1]/p/a[2]/text()').get(),
-                    'link' : car.xpath('article/div[2]/div[2]/h2/a/@href').get(),
-                }
+                        'price' : self.formatPrice(car.xpath('article/div[2]/div[last()]/div/div/span/text()').get()),
+                        'company' : self.formatCompany(car.xpath('article/div[2]/div[1]/div[1]/span/text()').get()),
+                        'location' : car.xpath('article/div[2]/div[1]/p/a[2]/text()').get(),
+                        'link' : car.xpath('article/div[2]/div[2]/h2/a/@href').get(),
+                    }
+                except Exception as ex:
+                    print("Oops! " + str(ex))
+                    traceback.print_tb(ex.__traceback__)
+
 
         next_page = response.xpath('//*[@id="root"]/div[2]/main/div[3]/div[1]/div[3]/div/div[2]/a[last()]/@href').get()
         print('link: ' + next_page)
